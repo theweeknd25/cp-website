99775d8abb0fdaea0b0e10e3501294950948ce15
Harsh Pawar <harsh.pawar25247@gmail.com>
2026-08-08 15:52:00 +0530

    Using official GA4 snippet

--- src/config/analytics.ts
+++
@@ -1,8 +0,0 @@
-// ===============================================
-// MONARCH v1.0.0
-// Analytics Configuration
-// ===============================================
-
-export const analytics = {
-  measurementId: "G-XWF0QBDQCL",
-};
